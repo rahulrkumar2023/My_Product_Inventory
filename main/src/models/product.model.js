0c53afae2c4317d28c2ea36b455aeee2e28bebfb
rahulrkumar2023 <69042826+rahulrkumar2023@users.noreply.github.com>
2026-08-06 09:08:27 +0530

    Add files via upload

---
+++ main/src/models/product.model.js
@@ -0,0 +1,45 @@
+
+export default class ProductModel{
+    constructor(id , name , description , price , imageUrl){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl; 
+  
+    }
+
+    static get(){
+        return products;
+    }
+
+    static add(name  , description , price , imageUrl){
+      let newProduct = new ProductModel(products.length+1 , name , description , price , mageUrl);
+      products.push(newProduct)
+
+    }
+
+
+    static getById(id){
+      return products.find((p)=> p.id == id)
+    }
+
+    static update(productObj){
+      const index = products.findIndex((p)=> p.id == productObj.id);
+      products[index] = productObj;
+    }
+
+    static delete(id){
+      const index = products.findIndex((p) => p.id == id);
+      products.splice(index , 1);
+
+    }
+}
+
+var products = [
+  new ProductModel(1, 'Product 1', 'Description for Product 1', 19.99, 'https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg'),
+  new ProductModel(2, 'Product 2', 'Description for Product 2', 29.99, 'https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg'),
+  new ProductModel(3, 'Product 3', 'Description for Product 3', 39.99, 'https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg')
+];
+
+
